517ed85ea2fe901639063f54be740495bc61cc27
GitHub Action <action@github.com>
2025-07-15 02:28:15 +0000

    schiba 0.2.7

--- Formula/schiba.rb
+++ Formula/schiba.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Schiba < Formula
   desc "Extract database schema in a compact format for AI chat context"
   homepage "https://github.com/kennylwx/schiba"
-  url "https://registry.npmjs.org/schiba/-/schiba-0.2.6.tgz"
-  sha256 "da68bd1da7255fa0be09d36fdcc5d29fe9836b7e043a656161b913e4499b8d2e"
+  url "https://registry.npmjs.org/schiba/-/schiba-0.2.7.tgz"
+  sha256 "75acc3839957c119479f32b4c9f84c43a4740c9b80d7e575b490c457a0083c1b"
   license "MIT"
 
   depends_on "node"
